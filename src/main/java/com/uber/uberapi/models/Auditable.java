7762c7eb3b0880d1fb61fb576e0300e8b5c56c47
Pragy Agarwal <agar.pragy@gmail.com>
2020-10-29 23:35:53 +0530

    add comments to Auditable

--- src/main/java/com/uber/uberapi/models/Auditable.java
+++ src/main/java/com/uber/uberapi/models/Auditable.java
@@ -10,11 +10,24 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
+// types of inheritance are available for databases
+// OOP <-/-> databases
+// single-table inheritance - all the columns from all the subclasses are present in 1 table
+//                          - disadvantage: sparse table
+// per-table-inheritance - each subclass has its own table with a copy of the parent classes columns
+//                       - lose our OOP - references cannot be made to the superclass
+// composition-based-inheritance - each subclass has a foreign key to the superclass object
+//                       - one of the best ways
+//                       - disadvantage - we have to use table joins - expensive
+// mapped-super-class - the super class is abstract - no table for the superclass
+
 @MappedSuperclass // don't create table for Auditable
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 public abstract class Auditable implements Serializable {
+    // sending these entities over network
+    // saving them in file
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -22,19 +35,36 @@ public abstract class Auditable implements Serializable {
     @Column(updatable = false)
     @Temporal(TemporalType.TIMESTAMP)  // Jpa
     @CreatedDate // hibernate
-    private Date createAt;
+    private Date createAt; // whenever the row is created - automatically set this value
 
     @Temporal(TemporalType.TIMESTAMP)
     @LastModifiedDate // hibernate
-    private Date updatedAt;
+    private Date updatedAt; // whenever the row is modified - automatically update this
+
+    // Passenger() <-> row in the database
+    // Hibernate will cache things properly
+    // that entity comparison is based on the table and id instead of just the memory address
+
+    // when an entity is created - can spring provide the id for it?
+    // no - id is provided by the database
+    // Booking booking = new Booking();
+    // as long as it is just in memory
+    // booking.id == null
+    // bookingRepository.save(booking);
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
+        if (this == o) return true; // memory locations are same
+        // first make sure that they belong to the same class
         if (o == null || getClass() != o.getClass()) return false;
-
+        // other object o, interpret as an auditable object
         Auditable auditable = (Auditable) o;
+        // if any of the ids is null
+        // id = null, o.id = null   - both objects are in memory
+        // id = null, ob.id != null - can't be equal
+        // id != null, ob.id = null - can't be equal
         if (id == null || auditable.id == null) return false;
+        // both ids are non-null
         return id.equals(auditable.id);
     }
 
